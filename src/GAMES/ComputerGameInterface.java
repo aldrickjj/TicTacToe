305d21a2732aec1037080908cdc1b44ab6c611ae
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-03-26 01:17:14 -0400

    Major change to HumanGameInterface and ComputerGameInterface

--- src/GAMES/ComputerGameInterface.java
+++ src/GAMES/ComputerGameInterface.java
@@ -4,11 +4,14 @@ public class ComputerGameInterface implements GameInterface {
     private Player player, pc;
     private String[][] board = null;
     private Player term = null;
+    private Player winner = null;
+    private int moveNumber = 0;
+    private boolean gameOver;
 
     public ComputerGameInterface(int boardDimension) {
         this.player = createPlayer("Player", "X");
         this.pc = createPlayer("Computer", "O");
-        this.board = new String[boardDimension][boardDimension];
+        this.board = new String[5][5];
     }
 
     @Override
@@ -36,8 +39,110 @@ public class ComputerGameInterface implements GameInterface {
         }
     }
 
+    private boolean isMoveLegal(int x, int y) {
+        return this.board[y][x].equals(" ");
+    }
+
+    public boolean makeTerm(int x, int y) {
+        String symbol = this.term.getSymbol();
+        if(isMoveLegal(x, y)){
+            return false;
+        }
+        this.board[y][x] = symbol;
+        return true;
+    }
+
+    private boolean isGameOver() {
+        String symbol = this.term.getSymbol();
+        for(int i = 0; i < board.length; i += 1) {
+            if(checkHorizontal(symbol, i) || checkVertical(symbol, i)) {
+                return true;
+            }
+        }
+        if(checkDiagonal(symbol, "right") || checkDiagonal(symbol, "left")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkHorizontal(String symbol, int index) {
+        int num_symbol = 0;
+        for(int i = 0; i < board[index].length; i += 1) {
+            if(i >= 1 && !board[index][i].equals(symbol)) {
+                return false;
+            }
+            if(board[index][i].equals(symbol)) {
+                num_symbol += 1;
+                if(num_symbol == 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonal(String symbol, String side) {
+        if(side.equalsIgnoreCase("right")) {
+            if(board[0][3].equals(symbol) && board[1][2].equals(symbol) && board[2][1].equals(symbol) && board[3][0].equals(symbol)) {
+                return true;
+            }
+            else if (board[1][3].equals(symbol) && board[2][2].equals(symbol) && board[3][1].equals(symbol)) {
+                if(board[0][4].equals(symbol) || board[4][0].equals(symbol)) {
+                    return true;
+                }
+            }
+            else if(board[1][4].equals(symbol) && board[2][3].equals(symbol) && board[3][2].equals(symbol) && board[4][1].equals(symbol)) {
+                return true;
+            }
+        }
+        else if(side.equalsIgnoreCase("left")) {
+            if(board[0][1].equals(symbol) && board[1][2].equals(symbol) && board[2][3].equals(symbol) && board[3][4].equals(symbol)) {
+                return true;
+            }
+            else if (board[1][1].equals(symbol) && board[2][2].equals(symbol) && board[3][3].equals(symbol)) {
+                if(board[0][0].equals(symbol) || board[4][4].equals(symbol)) {
+                    return true;
+                }
+            }
+            else if(board[1][0].equals(symbol) && board[2][1].equals(symbol) && board[3][2].equals(symbol) && board[4][3].equals(symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkVertical(String symbol, int index) {
+        int num_symbol = 0;
+        for(int i = 0; i < board.length; i += 1) {
+            if(i >= 1 && !board[i][index].equals(symbol)) {
+                return false;
+            }
+            if(board[i][index].equals(symbol)) {
+                num_symbol += 1;
+                if(num_symbol == 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void changeTerm() {
+        if(moveNumber % 2 == 0) {
+            term = player;
+        }
+        else {
+            term = pc;
+        }
+    }
+
     @Override
     public void startGame() {
 
+    }
+
+    @Override
+    public Player getWinner() {
+        return this.winner;
     }
 }
--- src/GAMES/HumanGameInterface.java
+++ src/GAMES/HumanGameInterface.java
@@ -5,12 +5,13 @@ public class HumanGameInterface implements GameInterface {
     private String[][] board = null;
     private Player term = null;
     private Player winner = null;
+    private int moveNumber = 0;
     private boolean gameOver;
 
-    public HumanGameInterface(int boardDimension) {
+    public HumanGameInterface() {
         this.player1 = createPlayer("Player 1", "X");
         this.player2 = createPlayer("Player 2", "O");
-        this.board = new String[boardDimension][boardDimension];
+        this.board = new String[5][5];
         this.gameOver = false;
     }
 
@@ -53,13 +54,108 @@ public class HumanGameInterface implements GameInterface {
     }
 
     private boolean isGameOver() {
-        
+        String symbol = this.term.getSymbol();
+        for(int i = 0; i < board.length; i += 1) {
+            if(checkHorizontal(symbol, i) || checkVertical(symbol, i)) {
+                return true;
+            }
+        }
+        if(checkDiagonal(symbol, "right") || checkDiagonal(symbol, "left")) {
+            return true;
+        }
         return false;
+    }
+
+    private boolean checkHorizontal(String symbol, int index) {
+        int num_symbol = 0;
+        for(int i = 0; i < board[index].length; i += 1) {
+            if(i >= 1 && !board[index][i].equals(symbol)) {
+                return false;
+            }
+            if(board[index][i].equals(symbol)) {
+                num_symbol += 1;
+                if(num_symbol == 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonal(String symbol, String side) {
+        if(side.equalsIgnoreCase("right")) {
+            if(board[0][3].equals(symbol) && board[1][2].equals(symbol) && board[2][1].equals(symbol) && board[3][0].equals(symbol)) {
+                return true;
+            }
+            else if (board[1][3].equals(symbol) && board[2][2].equals(symbol) && board[3][1].equals(symbol)) {
+                if(board[0][4].equals(symbol) || board[4][0].equals(symbol)) {
+                    return true;
+                }
+            }
+            else if(board[1][4].equals(symbol) && board[2][3].equals(symbol) && board[3][2].equals(symbol) && board[4][1].equals(symbol)) {
+                return true;
+            }
+        }
+        else if(side.equalsIgnoreCase("left")) {
+            if(board[0][1].equals(symbol) && board[1][2].equals(symbol) && board[2][3].equals(symbol) && board[3][4].equals(symbol)) {
+                return true;
+            }
+            else if (board[1][1].equals(symbol) && board[2][2].equals(symbol) && board[3][3].equals(symbol)) {
+                if(board[0][0].equals(symbol) || board[4][4].equals(symbol)) {
+                    return true;
+                }
+            }
+            else if(board[1][0].equals(symbol) && board[2][1].equals(symbol) && board[3][2].equals(symbol) && board[4][3].equals(symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkVertical(String symbol, int index) {
+        int num_symbol = 0;
+        for(int i = 0; i < board.length; i += 1) {
+            if(i >= 1 && !board[i][index].equals(symbol)) {
+                return false;
+            }
+            if(board[i][index].equals(symbol)) {
+                num_symbol += 1;
+                if(num_symbol == 4) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void changeTerm() {
+        if(moveNumber % 2 == 0) {
+            term = player1;
+        }
+        else {
+            term = player2;
+        }
     }
 
     @Override
     public void startGame() {
         this.term = player1;
         clearBoard();
+        while (!gameOver) {
+            changeTerm();
+            moveNumber += 1;
+            int x = 0;//to be changed
+            int y = 0;//to be changed
+            while(!makeTerm(x, y)) {
+                //Do something here
+            }
+            gameOver = isGameOver();
+        }
+        winner = this.winner;
+    }
+
+    @Override
+    public Player getWinner() {
+        return this.winner;
     }
 }
