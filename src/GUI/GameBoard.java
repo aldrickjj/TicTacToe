49621483ce0dbb349a9d58f17b825b5d30b8f2dd
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-01 19:33:08 -0400

    added main and start to GameBorad.java

--- src/GUI/GameBoard.java
+++ src/GUI/GameBoard.java
@@ -1,4 +1,19 @@
 package GUI;
 
-public class GameBoard {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class GameBoard extends Application{
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        
+    }
 }
