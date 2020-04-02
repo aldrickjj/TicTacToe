cf39f0a1cbef014e2f260f2291b5fc35a1b75767
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-01 20:48:53 -0400

    Deleted table image from the project. Replace table image with lines in GameBoard.fxml

--- src/GUI/GameBoard.java
+++ src/GUI/GameBoard.java
@@ -11,6 +11,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+
 public class GameBoard extends Application{
 
     public static void main(String[] args) {launch(args);}
@@ -19,6 +22,7 @@ public class GameBoard extends Application{
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
         stage.setScene(new Scene(root));
+        stage.setTitle("Tic Tac Toe!");
         stage.show();
     }
 }
