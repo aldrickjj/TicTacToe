1ee5c6a2172b33d9e3eb63df9446ef9d31da8506
weiwang928 <62563971+weiwang928@users.noreply.github.com>
2020-04-01 20:28:59 -0400

    Add table image to the repository. Edited layout for GameBoard.fxml. added table image and two buttons to the fxml file.Edited start method in GameBoard.java to display GameBoard.fxml

--- src/GUI/GameBoard.java
+++ src/GUI/GameBoard.java
@@ -3,7 +3,10 @@ package GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -14,6 +17,8 @@ public class GameBoard extends Application{
 
     @Override
     public void start(Stage stage) throws Exception{
-        
+        Parent root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
